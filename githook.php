81450ad42d724cb1852ebfc1091cfc965035e284
Bence Meszaros <bence.me@gmail.com>
2015-02-02 13:51:27 -0500

    githook.php changed a little again

--- githook.php
+++ githook.php
@@ -2,6 +2,11 @@
 
 ignore_user_abort(true);
 
-if (!empty($_REQUEST['payload'])) {
-	$result = `git pull`;
+if (!empty($_REQUEST['payload']))
+{
+        $full_result = NULL;
+        $return_value = NULL;
+        $short_result = exec('git pull', $full_result, $return_value);
+
+        //TODO: check and report errors in a meaningful way
 }
