2c17184bcedf8bfba75eb4cf2f20b19fe5662e27
Bence Meszaros <bence.me@gmail.com>
2015-02-05 10:45:39 -0500

    Modernized githook.php a little bit

--- githook.php
+++ githook.php
@@ -4,9 +4,42 @@ ignore_user_abort(true);
 
 if (!empty($_REQUEST['payload']))
 {
-        $full_result = NULL;
-        $return_value = NULL;
-        $short_result = exec('git pull', $full_result, $return_value);
+	$pipes = NULL;
+	$p_resource = proc_open('git pull', array(
+		array('pipe', 'r'), array('pipe', 'w'), array('pipe', 'w')
+	), $pipes);
 
-        //TODO: check and report errors in a meaningful way
+
+	if ($p_resource === FALSE)
+	{
+		echo 'Failed to open \'git pull\' process.' . "\n";
+	}
+	else
+	{
+		fclose($pipes[0]);
+
+		$stdout = stream_get_contents($pipes[1]);
+		fclose($pipes[1]);
+
+		$stderr = stream_get_contents($pipes[2]);
+		fclose($pipes[2]);
+
+		$return_value = proc_close($p_resource);
+
+		if (!empty($stdout))
+		{
+			echo 'stdout: <pre>' . $stdout . '</pre>' . "\n";
+		}
+
+		if ($return_value !== 0)
+		{
+			echo 'error: <pre>' . $return_value . '</pre>' . "\n";
+
+
+			if (!empty($stderr))
+			{
+				echo 'stderr: <pre>' . $stderr . '</pre>' . "\n";
+			}
+		}
+	}
 }
